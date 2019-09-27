a95bf07e5c7f8801c404b4202820050a91fc2646
Ali <ali@opnetwork.gg>
2019-09-27 17:24:50 +0300

    Added CHR and Item spec json extraction

--- src/convertor.js
+++ src/convertor.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
+const path = require('path')
 const THREE = require('three')
 const GLTFExporter = require('./extractors/gltf')
 const loadWLD = require('./loaders/wld')
 const loadMesh = require('./loaders/mesh')
+const { promisify } = require('util')
 
 module.exports = async function convertS3D(s3dName, type, s3d, out) {
   if (type === "chr") {
@@ -28,6 +30,7 @@ module.exports = async function convertS3D(s3dName, type, s3d, out) {
 }
 
 async function convertZoneToglTF(zoneName, s3d, out) {
+  console.log(`Extracting ${zoneName}.s3d`)
   let wld = s3d.files[`${zoneName}.wld`]
   let obj = s3d.files['objects.wld']
   let zone = loadWLD(wld)
@@ -67,16 +70,11 @@ async function convertZoneToglTF(zoneName, s3d, out) {
     }
   }
   scene.userData.objectLocations = objectLocations
-  const exporter = new GLTFExporter()
-  await exporter.parse(scene, gltf => {
-    fs.writeFileSync(`${out}/${zoneName}.glb`, Buffer.from(gltf))
-  }, {
-    embedImages: false,
-    binary: true
-  })
+  await convertToGltf(scene, out, zoneName, 0)
 }
 
 async function convertObjToglTFs(zoneName, s3d, out) {
+  console.log(`Extracting ${zoneName}_obj.s3d`)
   let wld = s3d.files[`${zoneName}_obj.wld`]
   let zone = loadWLD(wld)
   let scene = new THREE.Scene()
@@ -94,16 +92,11 @@ async function convertObjToglTFs(zoneName, s3d, out) {
       }
     }
   }
-  const exporter = new GLTFExporter()
-  await exporter.parse(scene, gltf => {
-    fs.writeFileSync(`${out}/${zoneName}_obj.glb`, Buffer.from(gltf))
-  }, {
-    embedImages: false,
-    binary: true
-  })
+  await convertToGltf(scene, out, zoneName, 1)
 }
 
 async function convertChrToglTFs(zoneName, s3d, out) {
+  console.log(`Extracting ${zoneName}_chr.s3d`)
   let wld = zoneName.indexOf('gequip') !== -1 ? s3d.files[`${zoneName}.wld`] : s3d.files[`${zoneName}_chr.wld`]
   let zone = loadWLD(wld)
   let zoneKeys = Object.keys(zone)
@@ -147,21 +140,7 @@ async function convertChrToglTFs(zoneName, s3d, out) {
         let mesh = loadMesh(zone[mesh0.mesh], zone, materialCache, imageCache)
         scene.add(mesh)
       }
-      const exporter = new GLTFExporter()
-      await exporter.parse(scene, gltf => {
-        if (gltf instanceof ArrayBuffer) {
-          let data
-          try {
-            data = Buffer.from(gltf)
-          } catch(err) {
-            throw new Error(err)
-          }
-          fs.writeFileSync(`${out}/${raceCode}.glb`, Buffer.from(gltf))
-        }
-      }, {
-        embedImages: false,
-        binary: true
-      })
+      await convertToGltf(scene, out, raceCode, 2)
     }
   }
 }
@@ -186,3 +165,82 @@ function walkSkeleton(chr, entries, bone, parentShift = new THREE.Vector3(), par
   }
 }
 
+async function convertToGltf(scene, out, zoneName, type) {
+  await new Promise((resolve) => {
+    const exporter = new GLTFExporter()
+    exporter.parse(scene, gltf => {
+      fs.writeFileSync(`${out}/${zoneName}${type === 2 ? '_chr' : type === 1 ? '_obj' : ''}.glb`, Buffer.from(gltf))
+      resolve()
+    }, {
+      embedImages: false,
+      binary: true
+    })
+  })
+}
+
+async function exportCharModelSpecs(raceCode, data, out) {
+  try {
+    let files = fs.readdirSync(path.join(out, 'textures'))
+    const buf = Buffer.from(data)
+    const jsonBufSize = buf.readUInt32LE(12)
+    const jsonString = buf.toString('utf8', 20, jsonBufSize + 20)
+    const gltf = JSON.parse(jsonString)
+    const images = gltf.images
+    const imgs = images.map(img => {
+      return path.basename(img.uri)
+    })
+    let maxHelm = 0
+    let maxBody = 0
+    let bodyImage
+    gltf.nodes.forEach(node => {
+      if (node.name.substr(3, 2) === "HE") {
+        if (parseInt(node.name.substr(5, 2)) > maxHelm) {
+          maxHelm = parseInt(node.name.substr(5, 2))
+        }
+      } else if (node.name.substr(3, 1) === "0") {
+        bodyImage = imgs[gltf.materials[gltf.meshes[node.mesh].primitives[0].material].pbrMetallicRoughness.baseColorTexture.index]
+        if (parseInt(node.name.substr(3, 2)) > maxBody) {
+          maxBody = parseInt(node.name.substr(3, 2))
+        }
+      }
+    })
+    let imageSpecs = {}
+    imgs.forEach(img => {
+      let raceFile = img.indexOf(raceCode.toLowerCase()) !== -1
+      let alpha = img.indexOf('alpha') !== -1
+      let partFiles = files.filter(value => value.substr(0, raceFile ? 5 : 3) == img.substr(0, raceFile ? 5 : 3) && value.substr(alpha ? value.length - 11 : value.length - 5, 1) === img.substr(alpha ? img.length - 11 : img.length - 5, 1) && (alpha ? value.indexOf('alpha') !== -1 : value.indexOf('alpha') === -1))
+      let maxTexture = 0
+      let maxFace = 0
+      partFiles.forEach(file => {
+        let startNumbers = partFiles[0].search(/[0-9]/)
+        let texture = parseInt(file.substr(startNumbers < 0 ? 0 : startNumbers, 2))
+        if (!isNaN(texture) && texture > maxTexture) {
+          maxTexture = texture
+        }
+        if (img.substr(3, 2) === "he" && parseInt(file.substr(7, 1)) > maxFace) {
+          maxFace = parseInt(file.substr(7, 1))
+        }
+      })
+      imageSpecs[img] = {
+        ...(img.substr(3,2) === "he" ? {maxFace} : {}),
+        maxTexture
+      }
+    })
+    let keys = Object.keys(imageSpecs)
+    let basePart = keys.filter(value => value.indexOf(raceCode.toLowerCase()) !== -1 && value.indexOf('he', 3) === -1)[0]
+    if (!basePart) basePart = keys[0]
+    let baseHead = keys.filter(value => value.indexOf(raceCode.toLowerCase()) !== -1 && value.indexOf('he', 3) !== -1)[0]
+    let modelSpec = {
+      maxFace: baseHead ? imageSpecs[baseHead].maxFace : 0,
+      maxHelm,
+      maxTexture: keys.length > 0 ? basePart ? imageSpecs[basePart].maxTexture : imageSpecs[0].maxTexture : 0,
+      maxBodyTexture: bodyImage ? imageSpecs[bodyImage].maxTexture + 6 : 0,
+      maxBody,
+      imageSpecs
+    }
+    console.log(`${out}/${raceCode}.json`)
+    fs.writeFileSync(`${out}/${raceCode}.json`, JSON.stringify(modelSpec))
+  } catch(err) {
+    throw new Error(err)
+  }
+}
